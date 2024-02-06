4273816486c5cce9e6963b02d7ef14c6372c52db
Bret Johnson <bret.johnson@microsoft.com>
2024-02-06 11:56:58 -0500

    Expanded template for MauiAspire
    This expansion done by the (now removed) Expand-Template.ps1 script.

--- test/MauiAspire.Tests/CalculatorTests.cs
+++ test/MauiAspire.Tests/CalculatorTests.cs
@@ -1,7 +1,7 @@
-﻿// Copyright (c) COMPANY-PLACEHOLDER. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Library;
+using MauiAspire;
 using Xunit;
 
 public class CalculatorTests
