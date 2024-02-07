f61f66d437be1cf4c560254ea3d35e619b33e6c0
Bret Johnson <bret.johnson@microsoft.com>
2024-02-07 04:07:29 -0500

    Add all projects

---
+++ src/MauiAspire.App/WrapperMauiAppBuilder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.Metrics;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MauiAspire.App;
+
+public class WrapperMauiAppBuilder : IHostApplicationBuilder
+{
+    private readonly MauiAppBuilder _appBuilder;
+
+    public WrapperMauiAppBuilder(MauiAppBuilder appBuilder)
+    {
+        _appBuilder = appBuilder;
+    }
+
+    public IConfigurationManager Configuration => _appBuilder.Configuration;
+
+    public IHostEnvironment Environment => new MauiHostEnvironment();
+
+    public ILoggingBuilder Logging => _appBuilder.Logging;
+
+    public IMetricsBuilder Metrics => null;
+
+    public IDictionary<object, object> Properties => throw new NotImplementedException();
+
+    public IServiceCollection Services => _appBuilder.Services;
+
+    public void ConfigureContainer<TContainerBuilder>(IServiceProviderFactory<TContainerBuilder> factory, Action<TContainerBuilder>? configure = null) where TContainerBuilder : notnull
+    {
+        _appBuilder.ConfigureContainer<TContainerBuilder>(factory, configure);
+    }
+
+    public MauiApp Build()
+    {
+        return _appBuilder.Build();
+    }
+}
---
+++ src/MauiAspire.ProjectTemplates/templates/maui-aspire/AspireStarterApplication.1.MauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientAppsIntegration.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
