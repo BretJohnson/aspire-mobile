e81db3ce10a25345330e0f66efc250453066dcee
Bret Johnson <bretjohn@microsoft.com>
2024-03-01 01:32:40 -0500

    Update doc comment

--- src/AspireMobile.Hosting/MobileProjectResourceBuilderExtension.cs
+++ src/AspireMobile.Hosting/MobileProjectResourceBuilderExtension.cs
@@ -5,10 +5,13 @@ namespace Aspire.Hosting;
 public static class MobileProjectResourceBuilderExtension
 {
     /// <summary>
-    /// Adds a .NET project to the application model. 
+    /// Adds a .NET mobile project (for example, a .NET MAUI project) to the application model. These projects are
+    /// different than other Aspire managed projects. They aren't launched by Aspire itself, so Aspire can't pass
+    /// settings to the app via environment variables. Instead those environment settings are written to a config
+    /// file (AspireAppSettings.g.cs by default) which is included in the app at build time.
     /// </summary>
     /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/>.</param>
-    /// <param name="name">The name of the resource. This name will be used for service discovery when referenced in a dependency.</param>
+    /// <param name="name">The name of the resource. This name shows in the dashboard and can be used for service discovery when referenced in a dependency.</param>
     /// <param name="projectDirectory">The path to the project file.</param>
     /// <returns>A reference to the <see cref="IResourceBuilder{ProjectResource}"/>.</returns>
     public static IResourceBuilder<ProjectResource> AddMobileProject(this IDistributedApplicationBuilder builder, string name, string projectDirectory,
