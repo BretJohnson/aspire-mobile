19b9dfd57a73ac9489b1cb34bc1b318923e2c171
Bret Johnson <bretjohn@microsoft.com>
2024-03-01 12:31:42 -0500

    Fix namespace

--- src/MauiAspire.ProjectTemplates/templates/maui-aspire/AspireStarterApplication.1/MauiProgram.cs
+++ src/MauiAspire.ProjectTemplates/templates/maui-aspire/AspireStarterApplication.1/MauiProgram.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
-namespace AspireStarterApplication._1.MauiApp;
+namespace AspireStarterApplication._1;
 
 public static class MauiProgram
 {
